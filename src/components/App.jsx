aa5e8de76c80127b3f2d5dcf9e301af51b7d7fd0
ArIonMark1 <andreydatsenko777@gmail.com>
2023-06-21 19:57:53 +0200

    task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import debounce from 'lodash.debounce';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
@@ -6,16 +6,54 @@ import SearchContacts from './SearchContacts/SearchContacts';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// const DATA_STATE = {
-//   localKey: 'contacts',
-//   contacts: [],
-//   filter: '',
-// };
-
 export default function App() {
-  const [contacts, setContacts] = useState();
-  const [filter, setFilter] = useState();
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
   const LOCAL_KEY = 'contacts';
+  // ====================================
+  //   componentDidMount()
+  useEffect(() => {
+    const dataStorage = localStorage.getItem(LOCAL_KEY);
+    const parsedDataStorage = JSON.parse(dataStorage);
+    //
+    if (parsedDataStorage && parsedDataStorage.length > 0) {
+      // додаткова перевірка, як що користувач видалив записи лишається пустий масив але [] === true !!
+      setContacts([...parsedDataStorage]);
+    }
+  }, []);
+  // componentDidUpdate()
+  useEffect(() => {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  // ********************************************
+
+  function receiveFormData(data) {
+    if (
+      contacts &&
+      contacts.find(
+        contact => contact.name === data.name && contact.number === data.number
+      )
+    ) {
+      return toast.error(`${data.name} is already in contacts.`);
+    }
+    setContacts([...contacts, data]);
+    // contacts: [...contacts, data];
+    toast.success(`${data.name} added in contacts.`);
+  }
+  // //******************************************** */
+
+  const filterInputData = debounce(data => setFilter(data.toLowerCase()), 400);
+
+  function filteredContacts() {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    ); // повертає новий масив
+  }
+
+  function deleteContact(id) {
+    setContacts(contacts => contacts.filter(contact => contact.id !== id));
+  }
 
   return (
     <div className="container">
@@ -32,106 +70,13 @@ export default function App() {
         pauseOnHover
         theme="light"
       />
-      <ContactForm onSubmit={this.receiveFormData} />
-      {this.state.contacts.length !== 0 && (
+      <ContactForm onSubmit={receiveFormData} />
+      {contacts.length !== 0 && (
         <>
-          <SearchContacts onChange={this.filterInputData} />
-          <ContactList
-            contacts={this.filteredContacts()}
-            onDelete={this.deleteContact}
-          />
+          <SearchContacts onChange={filterInputData} />
+          <ContactList contacts={filteredContacts()} onDelete={deleteContact} />
         </>
       )}
     </div>
   );
 }
-
-// export default class App extends PureComponent {
-//   state = {
-//     ...DATA_STATE,
-//   };
-
-//   componentDidMount() {
-//     const dataStorage = localStorage.getItem([this.state.localKey]);
-//     //
-//     const parsedDataStorage = JSON.parse(dataStorage);
-//     if (parsedDataStorage && parsedDataStorage.length > 0) {
-//       // додаткова перевірка, як що користувач видалив записи лишається пустий масив але [] === true !!
-//       this.setState({ [this.state.localKey]: [...parsedDataStorage] });
-//     }
-//   }
-
-//   componentDidUpdate(_, prevState) {
-//     if (this.state.contacts.length !== prevState.contacts.length) {
-//       localStorage.setItem(
-//         [this.state.localKey],
-//         JSON.stringify(this.state.contacts)
-//       );
-//     }
-//   }
-//   // ********************************************
-
-//   receiveFormData = data => {
-//     const { contacts } = this.state;
-
-//     if (
-//       contacts.find(
-//         contact => contact.name === data.name && contact.number === data.number
-//       )
-//     ) {
-//       return toast.error(`${data.name} is already in contacts.`);
-//     }
-//     this.setState({ contacts: [...contacts, data] });
-//     toast.success(`${data.name} added in contacts.`);
-//   };
-//   //******************************************** */
-//   filterInputData = debounce(data => {
-//     // console.log('from App: ', data);
-//     this.setState({ filter: data.toLowerCase() });
-//   }, 400);
-
-//   filteredContacts = () => {
-//     const { contacts, filter } = this.state;
-
-//     const filteredContactArr = contacts.filter(contact => {
-//       return contact.name.toLowerCase().includes(filter);
-//     });
-//     return filteredContactArr; // повертає новий масив
-//   };
-
-//   deleteContact = id => {
-//     this.setState(({ contacts }) => ({
-//       contacts: contacts.filter(contact => contact.id !== id),
-//     }));
-//   };
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <h2>Phone Book</h2>
-//         <ToastContainer
-//           position="top-center"
-//           autoClose={2000}
-//           hideProgressBar={false}
-//           newestOnTop={false}
-//           closeOnClick
-//           rtl={false}
-//           pauseOnFocusLoss
-//           draggable
-//           pauseOnHover
-//           theme="light"
-//         />
-//         <ContactForm onSubmit={this.receiveFormData} />
-//         {this.state.contacts.length !== 0 && (
-//           <>
-//             <SearchContacts onChange={this.filterInputData} />
-//             <ContactList
-//               contacts={this.filteredContacts()}
-//               onDelete={this.deleteContact}
-//             />
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
